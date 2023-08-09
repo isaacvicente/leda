8d2a4529c3ee19b1c8741e651996f60a30020cbf
Isaac Vicente <isaac.vicente.medeiros.silva@ccc.ufcg.edu.br>
2023-08-09 09:44:53 -0300

    Começa a fazer RR3

--- RR3-01-Roteiro-Revisao-JUnit/src/main/java/recursao/MetodosRecursivos.java
+++ RR3-01-Roteiro-Revisao-JUnit/src/main/java/recursao/MetodosRecursivos.java
@@ -9,27 +9,17 @@ public class MetodosRecursivos {
 		return result;
 	}
 	public long calcularFatorial(int n) {
-		long result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O FATORIAL DO PARAMETRO
-		// DE ACORDO COM O QUE FOI MOSTRADO NO EXERCCICIO. OBSERVE QUE SENDO O
-		// METODO
-		// RECURSIVO, O FATORIAL DOS NUMEROS ANTERIORES TAMBEM VAO SER IMPRESSOS
-		return result;
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return n * calcularFatorial(n - 1);
 	}
 
 	public int calcularFibonacci(int n) {
-		int result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O N-ESIMO TERMO
-		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
-		// PRIMEIRO E SEGUNDO NUMEROS
-		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
-		// SEQUENCIA É DADO
-		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
-		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
-		// IMPRESSOS
-		return result;
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+		return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
 	}
 
 	public int countNotNull(Object[] array) {
@@ -41,20 +31,17 @@ public class MetodosRecursivos {
 	}
 
 	public long potenciaDe2(int expoente) {
-		int result = 1;
-		// TODO IMPLEMENTE (USANDO RECURSAO) O CODIGO PARA PRODUZIR A N-ESIMA
-		// POTENCIA
-		// DE 2
-		return result;
+		if (expoente == 0) {
+			return 1;
+		}
+		if (expoente == 1) {
+			return 2;
+		}
+
+		return 2 * potenciaDe2(expoente - 1);
 	}
 
 	public double progressaoAritmetica(double termoInicial, double razao, int n) {
-		double result = 0;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO ARITMETICA, DADO O TERMO INICIAL E A RAZAO
-		// VOCE NAO PODE USAR A FORMULA QUE CALCULA O N-ESIMO TERMO. DEVE USAR RECURSAO
-		return result;
 	}
 
 	public double progressaoGeometrica(double termoInicial, double razao, int n) {
