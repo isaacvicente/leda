5286f089a7da29de25393c0a4176c15c5515e04f
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-08-12 15:24:19 -0300

    Resolve R1

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble/src/test/java/sorting/test/StudentSortingTest.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble/src/test/java/sorting/test/StudentSortingTest.java
@@ -11,6 +11,7 @@ import sorting.simpleSorting.BubbleSort;
 import sorting.simpleSorting.InsertionSort;
 import sorting.simpleSorting.SelectionSort;
 import sorting.variationsOfBubblesort.BidirectionalBubbleSort;
+import sorting.variationsOfBubblesort.RecursiveBubbleSort;
 import sorting.variationsOfSelectionsort.RecursiveSelectionSort;
 
 public class StudentSortingTest {
@@ -43,7 +44,7 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = new RecursiveSelectionSort<>();
+		this.implementation = new RecursiveBubbleSort<>();
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
