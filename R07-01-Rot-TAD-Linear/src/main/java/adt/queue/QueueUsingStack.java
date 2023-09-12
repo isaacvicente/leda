afa7bab834812a2412c9794bf513a6f83da14815
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-09-12 18:23:28 -0300

    Implementa `QueueUsingStack`
    A solução não foi testada

--- R07-01-Rot-TAD-Linear/src/main/java/adt/queue/QueueUsingStack.java
+++ R07-01-Rot-TAD-Linear/src/main/java/adt/queue/QueueUsingStack.java
@@ -2,6 +2,8 @@ package adt.queue;
 
 import adt.stack.Stack;
 import adt.stack.StackImpl;
+import adt.stack.StackOverflowException;
+import adt.stack.StackUnderflowException;
 
 public class QueueUsingStack<T> implements Queue<T> {
 
@@ -12,35 +14,97 @@ public class QueueUsingStack<T> implements Queue<T> {
 		stack1 = new StackImpl<T>(size);
 		stack2 = new StackImpl<T>(size);
 	}
+	
+	private void desempilhaParaStack2(Stack<T> stack1, Stack<T> stack2) {
+		while (!stack1.isEmpty()) {
+			T element = null;
+			try {
+				element = stack1.pop();
+			} catch (StackUnderflowException e) {
+				e.printStackTrace();
+			}			
+			try {
+				stack2.push(element);
+			} catch (StackOverflowException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void empilhaParaStack1(Stack<T> stack1, Stack<T> stack2) {
+		while (!this.stack2.isEmpty()) {
+			T element = null;
+			try {
+				element = this.stack2.pop();
+			} catch (StackUnderflowException e) {
+				e.printStackTrace();
+			}			
+			try {
+				this.stack1.push(element);
+			} catch (StackOverflowException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	@Override
 	public void enqueue(T element) throws QueueOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isFull()) {
+			throw new QueueOverflowException();
+		}
+
+		try {
+			this.stack1.push(element);
+		} catch (StackOverflowException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public T dequeue() throws QueueUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isEmpty()) {
+			throw new QueueUnderflowException();
+		}
+		
+		T head = null;
+
+		desempilhaParaStack2(stack1, stack2);
+
+		try {
+			head = this.stack2.pop();
+		} catch (StackUnderflowException e) {
+			e.printStackTrace();
+		}
+
+		empilhaParaStack1(stack1, stack2);
+		
+		return head;
 	}
 
 	@Override
 	public T head() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (isEmpty()) {
+			return null;
+		}
+		T head;
+
+		desempilhaParaStack2(stack1, stack2);
+		
+		head = this.stack2.top();
+
+		empilhaParaStack1(stack1, stack2);
+		
+		return head;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.stack1.isEmpty();
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.stack1.isFull();
 	}
 
 }
